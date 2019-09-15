8bba93545a927d0b28bd6ee460f2c09ac76e7f79
lllbq <lllbq@outlook.com>
2019-09-15 20:07:17 +0800

    f

--- assets/js/katex.js
+++ assets/js/katex.js
@@ -25,7 +25,7 @@
     document.addEventListener("DOMContentLoaded", function () {
       renderMathInElement(document.body, {
         delimiters: [
-          { left: "$$", right: "$$", display: true },
+          { left: "$$", right: "$$", display: false },
           { left: "$", right: "$", display: false }
         ]
       });
